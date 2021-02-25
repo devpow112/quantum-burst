6dd21aba4b4f16488e9741cfee4ab275f32cb9a9
Devon Powell <devon.f.powell@gmail.com>
2021-02-24 20:34:07 -0500

    Add CI
    Will attempt a build in both Debug and Release as well as check formatting.

--- build.bat
+++ build.bat
@@ -12,20 +12,43 @@ if "%TYPE%" EQU "" (
   set "TYPE=release"
 )
 
+if "%TYPE%" EQU "Debug" (
+  set "TYPE=debug"
+) else if "%TYPE%" EQU "Release" (
+  set "TYPE=release"
+)
+
 if "%TYPE%" NEQ "debug" (
   if "%TYPE%" NEQ "asm" (
     if "%TYPE%" NEQ "release" (
-      exit /b 1
+      goto ERROR
     )
   )
 )
 
-copy /b "src\boot\rom_head.c" +,,"src\boot\rom_head.c" >NUL 2>&1
+copy /b "%CD%\src\boot\rom_head.c" +,,"%CD%\src\boot\rom_head.c" >NUL 2>&1
 
 if "%TYPE%" NEQ "asm" (
   "%GDK_WIN%\bin\make" -f "%GDK_WIN%\makefile.gen" clean
+  
+  if %ERRORLEVEL% NEQ 0 (
+    goto ERROR
+  )
 )
 
 "%GDK_WIN%\bin\make" -f "%GDK_WIN%\makefile.gen" %TYPE%
+
+if %ERRORLEVEL% NEQ 0 (
+  goto ERROR
+)
+  
+goto SUCCESS
+
+:ERROR
+
+popd
+exit /b 1
+
+:SUCCESS
 
 popd
