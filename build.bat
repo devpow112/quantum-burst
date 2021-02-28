8d3dbf39a81ece3a4b6ed9344208295570ce1386
Devon Powell <devon.f.powell@gmail.com>
2021-02-27 19:03:38 -0500

    Switch build script to powershell

--- build.bat
+++ build.bat
@@ -1,60 +1,16 @@
 @echo off
 
-pushd "%~dp0"
 set "TYPE=%1"
-set "GDK=%CD%/ext/sgdk"
-set "GDK=%GDK:\=/%"
-set "GDK_WIN=%CD%\ext\sgdk"
-set "SGCC=%CD%\ext\sgcc"
-set "PATH=%PATH%;%CD%\sgdk\bin"
+set "VERSION=%2"
 
 if "%TYPE%" EQU "" (
-  set "TYPE=release" 
-) else if /i "%TYPE%" EQU "debug" (
-  set "TYPE=debug"
-) else if /i "%TYPE%" EQU "release" (
-  set "TYPE=release"
-) else if /i "%TYPE%" EQU "clean" (
-  set "TYPE=clean"
-) else if /i "%TYPE%" EQU "asm" (
-  set "TYPE=asm"
-) else (
-  echo Incorrect build type. Must be debug, release, clean or asm
-  goto ERROR
+  set "TYPE=Release"
 )
 
-copy /b "%CD%\src\boot\rom_head.c" +,,"%CD%\src\boot\rom_head.c" >NUL 2>&1
-"%GDK_WIN%\bin\make" -f "%GDK_WIN%\makefile.gen" %TYPE%
-
-if %ERRORLEVEL% NEQ 0 (
-  echo Build failed!
-  goto ERROR
+if "%VERSION%" EQU "" (
+  set "VERSION=99"
 )
 
-if "%TYPE%" EQU "debug" (
-  goto CORRECT_CHECKSUM
-) else if "%TYPE%" EQU "release" (
-  goto CORRECT_CHECKSUM
-)
-
-goto END
-
-:CORRECT_CHECKSUM
-
-python "%SGCC%\sgcc.py" "out\rom.bin"
-
-if %ERRORLEVEL% NEQ 0 (
-  echo Checksum correction failed!
-  goto ERROR
-)
-
-goto END
-
-:ERROR
+pushd "%~dp0"
+powershell -File build.ps1 -BuildType %Type% -Version %VERSION%
 popd
-exit /b 1
-
-:END
-
-popd
-exit /b 0
---
+++ build.ps1
@@ -0,0 +1,51 @@
+param(
+  [Parameter(Mandatory=$false)][Int] $version = 99,
+  [Parameter(Mandatory=$false)][String] $buildType = 'Release'
+)
+
+$callingLocation = Get-Location
+
+try {
+  $buildType = $buildType.ToLower()
+
+  if (@('debug', 'release', 'clean', 'asm') -NotContains $buildType) {
+    throw "Invalid 'BuildType', allowed values are: debug, release, clean, asm"
+  }
+  
+  Set-Location $PSScriptRoot
+
+  $year4Digits = (Get-Date -Format 'yyyy')
+  $month3Letters = (Get-Date -Format 'MMM').ToUpper()
+  $version2Digits = ($version - 1).ToString().PadLeft(2,'0')
+  $headerContents = Get-Content -Path src\boot\rom_head.c.in
+  $headerContents = $headerContents.Replace('{Year4Digits}', $year4Digits)
+  $headerContents = $headerContents.Replace('{Month3Letters}', $month3Letters)
+  $headerContents = $headerContents.Replace('{Version2Digits}', $version2Digits)
+  
+  Set-Content -Path src\boot\rom_head.c -Value $headerContents  
+  
+  if (Test-Path -Path 'out\rom_cc.bin') {
+    Remove-Item 'out\rom_cc.bin'
+  }
+  
+  Invoke-Expression "ext\sgdk\bin\make -f ext\sgdk\makefile.gen $buildType"
+  
+  if ($lastExitCode -Ne 0 -Or -Not $?) {
+    throw "Build failed!"
+  }
+  
+  if (@('debug', 'release') -Contains $buildType) {
+    Write-Host 'Correcting checksum'
+    Invoke-Expression 'python ext\sgcc\sgcc.py out\rom.bin'
+  }
+  
+  if ($lastExitCode -Ne 0 -Or -Not $?) {
+    throw "Checksum correction failed!"
+  }
+} catch {
+  Write-Error $_
+
+  exit 1
+} finally {
+  Set-Location $callingLocation   
+}
