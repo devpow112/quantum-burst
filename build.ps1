743b5e679a3804cf95d36682130d1ec7da501960
Devon Powell <devon.f.powell@gmail.com>
2021-03-23 16:31:24 -0400

    Fix build issues
    Allows for repository path to have spaces in it.

--- build.ps1
+++ build.ps1
@@ -12,43 +12,53 @@ param(
 $callingLocation = Get-Location
 
 try {
-  Set-Location $PSScriptRoot
+  $buildRoot = $PSScriptRoot
 
   # generate ROM header
   $year4Digits = (Get-Date -Format 'yyyy')
   $month3Letters = (Get-Date -Format 'MMM').ToUpper()
   $revision2Digits = $revision.ToString().PadLeft(2, '0')
-  $contents = Get-Content -Path src\boot\rom_head.c.in
+  $contents = Get-Content -Path "$buildRoot\src\boot\rom_head.c.in"
   $contents = $contents.Replace('{Year4Digits}', $year4Digits)
   $contents = $contents.Replace('{Month3Letters}', $month3Letters)
   $contents = $contents.Replace('{Revision2Digits}', $revision2Digits)
 
-  Set-Content -Path src\boot\rom_head.c -Value $contents
+  Set-Content -Path "$buildRoot\src\boot\rom_head.c" -Value $contents
 
-  $build = @('debug', 'release') -Contains $buildType
+  $buildType = $buildType.ToLower()
+  $isBuild = @('debug', 'release') -Contains $buildType
 
   # remove checksum corrected ROM
-  if (Test-Path -Path 'out\rom_final.bin') {
-    Remove-Item 'out\rom_final.bin'
+  if ($isBuild -And $rebuild -Or $buildType -Eq 'clean') {
+    if (Test-Path -Path "$buildRoot\out\rom_final.bin") {
+      Remove-Item "$buildRoot\out\rom_final.bin"
+    }
   }
+
+  $extRoot = Join-Path $buildRoot ext
 
   # run build
-  $buildType = $buildType.ToLower()
+  Set-Location $buildRoot
 
-  if ($build -And $rebuild) {
-    Invoke-Expression "ext\sgdk\bin\make -f ext\sgdk\makefile.gen clean"
+  if ($isBuild -And $rebuild) {
+    & "$extRoot\sgdk\bin\make" -f "$extRoot\sgdk\makefile.gen" clean
+
+    if ($lastExitCode -Ne 0 -Or -Not $?) {
+      throw "Clean failed!"
+    }
   }
 
-  Invoke-Expression "ext\sgdk\bin\make -f ext\sgdk\makefile.gen $buildType"
+  & "$extRoot\sgdk\bin\make" -f "$extRoot\sgdk\makefile.gen" $buildType
 
   if ($lastExitCode -Ne 0 -Or -Not $?) {
     throw "Build failed!"
   }
 
   # correct ROM checksum
-  if ($build) {
-    Write-Host 'Correcting checksum'
-    Invoke-Expression 'python ext\sgcc\sgcc.py -s final out\rom.bin'
+  if ($isBuild) {
+    Write-Information 'Correcting checksum'
+
+    & python "$extRoot\sgcc\sgcc.py" -s final "$buildRoot\out\rom.bin"
   }
 
   if ($lastExitCode -Ne 0 -Or -Not $?) {
