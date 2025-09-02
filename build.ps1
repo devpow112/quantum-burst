b737c42a26766de3f18ebc2e0cdbc9f073c68ee2
Devon Powell <devon.f.powell@gmail.com>
2025-09-02 14:03:43 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -49,14 +49,14 @@ try {
   Set-Location $gameRoot
 
   if ($isBuild -And $rebuild) {
-    & "$sgdkRoot/bin/make" -f "$sgdkRoot/makefile.gen" clean
+    & "$sgdkRoot/bin/make.exe" -f "$sgdkRoot/makefile.gen" clean
 
     if ($lastExitCode -Ne 0 -Or -Not $?) {
       throw "Clean failed!"
     }
   }
 
-  & "$sgdkRoot/bin/make" -f "$sgdkRoot/makefile.gen" $buildType
+  & "$sgdkRoot/bin/make.exe" -f "$sgdkRoot/makefile.gen" $buildType
 
   if ($lastExitCode -Ne 0 -Or -Not $?) {
     throw "Build failed!"
